257b672226318827cffdecadb2b2b378af522300
yb-seo <44233919+yb-seo@users.noreply.github.com>
2019-10-16 14:57:09 +0900

    #10039. 평균 점수
    [구현]

---
+++ baekjoon/P10039.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P10039 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		
+		for (int i=0; i<5; i++) {
+			int score = sc.nextInt();
+			if (score < 40) sum += 40;
+			else sum += score;
+		}
+		
+		System.out.println(sum / 5);
+		sc.close();
+	}
+}
