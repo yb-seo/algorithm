3593a65c423312397b2afe76bc56360b0d0e46c8
yb-seo <44233919+yb-seo@users.noreply.github.com>
2019-10-31 10:45:29 +0900

    #2750. 수 정렬하기
    [정렬]

---
+++ baekjoon/P2750.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class P2750 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i=0; i<n-1; i++) {
+			for (int j=0; j<n-i-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+			
+//			for (int j=0; j<n; j++) {
+//				System.out.print(arr[j]+" ");
+//			}
+//			System.out.println();
+		}
+		
+		for (int i=0; i<n; i++) {
+			System.out.println(arr[i]);
+		}
+		sc.close();
+	}
+}
